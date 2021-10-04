024317bbfc372465f8615864e30957b9532c426a
Vishwa <67421829+hosathota@users.noreply.github.com>
2021-10-04 13:20:09 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using IBM.Data.DB2.Core;
 using System.IO;
 using System.Threading;
+using System.Diagnostics;
 
 namespace ConsoleApp
 {
@@ -9,8 +10,10 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Db2 .NET from OC 10");
-            System.Diagnostics.Process.Start("CMD.exe", "yum install libxml2");
+            Console.WriteLine("Welcome to Db2 .NET from OC 11");
+            //System.Diagnostics.Process.Start("CMD.exe", "yum install libxml2");
+            ExecuteCommand("sudo yum install libxml2");
+            Console.WriteLine("executed install command");
             Thread.Sleep(8000);
             
             Console.WriteLine("Current directory is "+Directory.GetCurrentDirectory());
@@ -25,6 +28,21 @@ namespace ConsoleApp
                 Console.WriteLine("Db2 Connection Error "+ex.Message);
             }
             Console.WriteLine("Complete");
+        }
+        
+        static void ExecuteCommand(string command)
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "/bin/bash";
+            proc.StartInfo.Arguments = "-c \" " + command + " \"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.Start();
+
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                Console.WriteLine(proc.StandardOutput.ReadLine());
+            }
         }
     }
 }
